b5dc03903dd62570e5268a911c8496075b67d884
rizalabdullah <ra160920@gmail.com>
2022-04-14 10:05:17 +0700

    test

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ require(`dotenv`).config();
 const express = require("express");
 const app = express();
 const path = require("path");
-//const productRouter = require("./app/product/routes");
-//const productRouterV2 = require("./app/product_v2/routes");
-const productRouterV3 = require("./app/product_v3/routes");
+const productRouter = require("./app/product/routes");
+const productRouterV2 = require("./app/product_v2/routes");
+//const productRouterV3 = require("./app/product_v3/routes");
 //const productRouterV4 = require("./app/product_v4/routes");
 const logger = require("morgan");
 const cors = require("cors");
@@ -16,10 +16,10 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use("/public", express.static(path.join(__dirname, "uploads")));
 app.use(logger(`dev`));
-//app.use("/api/v1", productRouter);
+app.use("/api/v1", productRouter);
 //app.use("/", productRouter);
-//app.use("/api/v2", productRouterV2);
-app.use(`/api/v3`, productRouterV3);
+app.use("/api/v2", productRouterV2);
+//app.use(`/api/v3`, productRouterV3);
 //app.use(`/api/v4`, productRouterV4);
 
 
@@ -30,11 +30,11 @@ app.use(`/api/v3`, productRouterV3);
 //    });
 //});
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-   app.get('*', (req, res) => {
-     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    });   
-  }
+//if (process.env.NODE_ENV === 'production') {
+ //   app.use(express.static('frontend/build'));
+ //  app.get('*', (req, res) => {
+ //    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+ //   });   
+ // }
 
 app.listen(port, () => console.log(`listening on port ${port}`));
