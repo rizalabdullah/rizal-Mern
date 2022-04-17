14db3afadf2aead03feb86bc879bd561a2ffb0d5
rizalabdullah <ra160920@gmail.com>
2022-04-17 16:41:35 +0700

    testing

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 require(`dotenv`).config();
-//require(`./config/mongoose`);
+require(`./config/mongoose`);
 const express = require("express");
 const app = express();
 const path = require("path");
-const productRouter = require("./app/product/routes");
-const productRouterV2 = require("./app/product_v2/routes");
-//const productRouterV3 = require("./app/product_v3/routes");
-//const productRouterV4 = require("./app/product_v4/routes");
+//const productRouter = require("./app/product/routes");
+//const productRouterV2 = require("./app/product_v2/routes");
+const productRouterV3 = require("./app/product_v3/routes");
+const productRouterV4 = require("./app/product_v4/routes");
 const logger = require("morgan");
 const cors = require("cors");
 const port = process.env.PORT || 5000 ;
@@ -16,11 +16,11 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use("/public", express.static(path.join(__dirname, "uploads")));
 app.use(logger(`dev`));
-app.use("/api/v1", productRouter);
+//app.use("/api/v1", productRouter);
 //app.use("/", productRouter);
-app.use("/api/v2", productRouterV2);
-//app.use(`/api/v3`, productRouterV3);
-//app.use(`/api/v4`, productRouterV4);
+//app.use("/api/v2", productRouterV2);
+app.use(`/api/v3`, productRouterV3);
+app.use(`/api/v4`, productRouterV4);
 
 
 //app.use((req,res,next) => {
